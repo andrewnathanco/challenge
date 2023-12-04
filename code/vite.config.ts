a8a6141148d17355cddf50b4f22da78465343a02
andrew cohen <cohen.andrew.n@gmail.com>
2023-12-04 16:19:22 -0500

    feat: add action

--- code/vite.config.ts
+++ code/vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-    basicSsl(),
-  ],
+  plugins: [solidPlugin()],
   server: {
     port: 3000,
   },
