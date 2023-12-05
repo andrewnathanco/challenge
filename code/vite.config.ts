b277754572c57124d3ce29d2e4fd0627ad1f8ffb
andrew cohen <cohen.andrew.n@gmail.com>
2023-12-05 14:11:46 -0500

    feat: change base path

--- code/vite.config.ts
+++ code/vite.config.ts
@@ -3,6 +3,7 @@ import solidPlugin from "vite-plugin-solid";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
+  base: process.env.BASE_PATH || "/",
   plugins: [solidPlugin(), basicSsl()],
   server: {
     port: 3000,
