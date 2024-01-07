a088aad2c61b372eee968833ebeb5a0b36dbeec4
andrew cohen <cohen.andrew.n@gmail.com>
2024-01-07 14:45:55 -0500

    feat: test

--- code/vite.config.ts
+++ code/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   base: process.env.BASE_PATH || "/",
-  start: {
-    ssr: false,
+  build: {
+    assetsDir: process.env.BASE_PATH?.substring(1) || "/",
   },
 });
